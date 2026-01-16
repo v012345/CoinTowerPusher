a65179ae27a84e5c3e3b35190d77a94157dd66bc
v012345 <v012345@163.com>
2026-01-16 17:37:54 +0800

    使用预制体动态生成

--- assets/scripts/CoinTowerManager.ts
+++ assets/scripts/CoinTowerManager.ts
@@ -3,41 +3,41 @@ const { ccclass, property } = _decorator;
 // import { CoinTower } from './CoinTower';
 @ccclass('CoinTowerManager')
 export class CoinTowerManager extends Component {
-    // @property(CoinTower)
-    // CoinTowerLeft: CoinTower;
+    @property(Node)
+    coinTowersNode: Node;
     // @property(CoinTower)
     // CoinTowerRight: CoinTower;
-    // @property(Prefab)
-    // CoinTowerPrefab: Prefab;
+    @property(Prefab)
+    coinTowerPrefab: Prefab;
     // @property(Node)
     // Actors: Node;
     // @property
     // CoinTowerNumOfOneSide: number = 20;
-    // start() {
-    //     this.spawnCoinTowers();
-    // }
+    start() {
+        this.spawnCoinTowers();
+    }
 
     // update(deltaTime: number) {
 
     // }
-    // spawnCoinTowers() {
-    //     const node = instantiate(this.CoinTowerPrefab);
-    //     const coinTower = node.getComponent(CoinTower);
-    //     const radius = coinTower.getRadius()
-    //     const CTS = [this.CoinTowerLeft, this.CoinTowerRight];
-    //     for (let ct of CTS) {
-    //         const pos = ct.node.getPosition();
-    //         for (let i = 1; i < this.CoinTowerNumOfOneSide; i++) {
-    //             const node = instantiate(this.CoinTowerPrefab);
-    //             node.setParent(this.Actors);     // 或 scene / Canvas  
-    //             node.setPosition(new Vec3(
-    //                 pos.x + radius * 2 * i,
-    //                 pos.y,
-    //                 pos.z
-    //             ));
-    //         }
-    //     }
-    // }
+    spawnCoinTowers() {
+        //     const node = instantiate(this.CoinTowerPrefab);
+        //     const coinTower = node.getComponent(CoinTower);
+        //     const radius = coinTower.getRadius()
+        //     const CTS = [this.CoinTowerLeft, this.CoinTowerRight];
+        //     for (let ct of CTS) {
+        //         const pos = ct.node.getPosition();
+        //         for (let i = 1; i < this.CoinTowerNumOfOneSide; i++) {
+        //             const node = instantiate(this.CoinTowerPrefab);
+        //             node.setParent(this.Actors);     // 或 scene / Canvas  
+        //             node.setPosition(new Vec3(
+        //                 pos.x + radius * 2 * i,
+        //                 pos.y,
+        //                 pos.z
+        //             ));
+        //         }
+        //     }
+    }
 }
 
 
