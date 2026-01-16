c7548638151434b70375cde0fd033db195ce2dfa
v012345 <v012345@163.com>
2026-01-16 15:42:44 +0800

    碰撞层

--- assets/scripts/prefabs/OneLayerOfCoins.ts
+++ assets/scripts/prefabs/OneLayerOfCoins.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, CylinderCollider, Node, RigidBody, Vec3 } from 'cc';
+import { _decorator, Component, CylinderCollider, Node, PhysicsGroup, RigidBody, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('OneLayerOfCoins')
@@ -7,9 +7,10 @@ export class OneLayerOfCoins extends Component {
         this.node.children.forEach(coin => {
 
             const rb = coin.addComponent(RigidBody);
+            rb.group = PhysicsGroup.Coin;
             rb.useGravity = true;
             rb.applyForce(new Vec3(0, 500, 0), coin.getWorldPosition());
-
+            
             const collider = coin.addComponent(CylinderCollider);
             collider.radius = 1.311;
             collider.height = 0.7;
