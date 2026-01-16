4dec49b17475dbb9e167d3908708f996a6497e32
v012345 <v012345@163.com>
2026-01-16 17:50:30 +0800

    动态生成塔

--- assets/scripts/CoinTowerManager.ts
+++ assets/scripts/CoinTowerManager.ts
@@ -1,6 +1,6 @@
 import { _decorator, Component, instantiate, Node, Prefab, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
-// import { CoinTower } from './CoinTower';
+import { CoinTower } from './prefabs/CoinTower';
 @ccclass('CoinTowerManager')
 export class CoinTowerManager extends Component {
     @property(Node)
@@ -21,8 +21,22 @@ export class CoinTowerManager extends Component {
 
     // }
     spawnCoinTowers() {
-        //     const node = instantiate(this.CoinTowerPrefab);
-        //     const coinTower = node.getComponent(CoinTower);
+        // 左右两边 x 的坐标
+        [12.5, -12.5].forEach(x => {
+            for (let i = 0; i < 17; i++) {
+                const coinTowernode = instantiate(this.coinTowerPrefab);
+                const coinTower = coinTowernode.getComponent(CoinTower);
+                coinTower.layerNum = 15 + i
+                coinTowernode.setParent(this.coinTowersNode);
+                coinTowernode.setPosition(new Vec3(
+                    x,
+                    0,
+                    12.14 * i
+                ));
+            }
+        });
+
+
         //     const radius = coinTower.getRadius()
         //     const CTS = [this.CoinTowerLeft, this.CoinTowerRight];
         //     for (let ct of CTS) {
