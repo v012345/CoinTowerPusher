12def1d76544a143a06c51edf80aafcaca8f1b2e
v012345 <v012345@163.com>
2026-01-19 09:33:40 +0800

    硬币预制体

---
+++ assets/scripts/prefabs/Coin.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Coin')
+export class Coin extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
