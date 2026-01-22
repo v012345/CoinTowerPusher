c4bc19ca8a5266e7f1dfd0c047e4b30fa02bf7f7
v012345 <v012345@163.com>
2026-01-22 17:40:24 +0800

    归纳事件

---
+++ assets/scripts/Event/Event.ts
@@ -0,0 +1,12 @@
+
+export class Event {
+    static CargoBedUpgrade = "CargoBedUpgrade"; // Upgrade cargo bed event
+    static SawBladeUpgrade = "SawBladeUpgrade"; // Upgrade saw blade event
+    static SpeedUpgrade = "SpeedUpgrade"; // Upgrade speed event
+    static TractorMove = "TractorMove"; // Tractor move event
+    static TractorStop = "TractorStop"; // Tractor stop event
+    static TractorMoveBack = "TractorMoveBack"; // Tractor move back event
+    static CargoBedIsFull = "CargoBedIsFull"; // Cargo bed is full event
+    static BalanceChanged = "BalanceChanged"; // Balance changed event
+
+}
